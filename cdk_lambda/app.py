8d83438580f375638ec14cdeacb9162833b7e60b
Scott White <swhite@verticalrelevance.com>
2022-06-15 12:04:38 -0400

    Remove pipeline CDK

--- cdk_lambda/app.py
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env python3
-
-import aws_cdk as cdk
-
-from ResiliencyFoundationLambda.resiliency_foundation_lambda import ResiliencyFoundationLambdaStack
-
-app = cdk.App()
-ResiliencyFoundationLambdaStack(app,"resiliency-foundation-lambda")
-app.synth()
