4f92ce5b053f265c28a5fd6ed761bc1c170a2d55
Joe Bowen <jbowen@verticalrelevance.com>
2023-02-15 09:44:12 -0700

    Fixing bug

--- experiment_code/experimentvr/__init__.py
+++ experiment_code/experimentvr/__init__.py
@@ -6,7 +6,7 @@ from chaoslib.discovery.discover import (discover_actions, discover_probes,
 from chaoslib.types import (DiscoveredActivities, Discovery)
 from logzero import logger
 
-__version__ = '0.3.27'
+__version__ = '0.3.28'
 __all__ = ["__version__", "discover"]
 
 
